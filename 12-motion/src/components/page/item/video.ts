5bcf418b5fc7f1e9fde525c8d74407291059ae6c
jinwook song <wlsdnr129@naver.com>
2024-07-05 21:24:48 +0900

    feat: PageItemComponent & Composable Interface

--- 12-motion/src/components/page/item/video.ts
+++ 12-motion/src/components/page/item/video.ts
@@ -26,7 +26,7 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
   // https://regexr.com/5l6nr
   private convertToEmbeddedURL(url: string): string {
     const regExp =
-      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
     const match = url.match(regExp);
     const videoId = match ? match[1] || match[2] : undefined;
     if (videoId) {
